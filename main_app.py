0803a0a3bcaf05ffda37bd2d272bde6375516783
Sergey Demidov <emailforwork.ds@gmail.com>
2021-07-07 17:59:25 +0300

    Add TODO for folder

--- main_app.py
+++ main_app.py
@@ -1,3 +1,5 @@
 with open('C://Users//Admin//PycharmProjects//camping food//сomponents//components.txt', 'rt', encoding='utf-8') as file:
     for line in file.read():
         print(line, end='')
+
+        #TODO: разобраться с папками
